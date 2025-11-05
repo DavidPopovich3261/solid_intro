7617efd322cbf571cebfdd519e4a0132a43e7dd1
DavidPopovich3261 <8534397@gmail.com>
2025-11-05 09:26:43 +0200

    s1

---
+++ S/s1.py
@@ -0,0 +1,18 @@
+
+
+class Book:
+    def __init__(self,title,author,content):
+        self.title=title
+        self.author=author
+        self.content=content
+
+    def save_to_file(self,filename):
+        with open(f'{filename}.txt', "a") as f:
+            f.write(f'{self.title},{self.author},{self.content}')
+
+
+
+class Save:
+    def __init__(self,filename,item):
+        with open(f'{filename}.txt', "a") as f:
+            f.write(f'{item.title},{item.author},{item.content}')
